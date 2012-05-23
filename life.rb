dd72d3e70ff7492a715955f5de5f494c1b4958af
Jeremy Rubin <jeremy.l.rubin@gmail.com>
2012-05-22 23:52:21 -0400

    I have made several changes to document the code better and to make it more ruby esque. I also added inputs for size and sparcity of cells. I also fixed the printing function.

--- life.rb
+++ life.rb
@@ -1,49 +1,53 @@
 class Game    
-    def initialize(xSize, ySize)
+    def initialize
+        puts "How large is the playing field? (input 179X49 for example)"
+        xSize, ySize = gets.split(/X/i) # case insensitive RegEx  to split input
+        xSize, ySize = 179, 49 if xSize || ySize == ''
+        puts "What percent of cells do you want filled?"
+        @sparsity = gets.to_f/100 # takes a percent input and converts to decimal
         @x = xSize.to_i
         @y = ySize.to_i
-        @dish = Array.new(@x, 0)
-        
-        0.upto(@x-1) {|i| 
-               @dish[i] = Array.new(@y, 0)
-            0.upto(@y-1){ |j| @dish[i][j] = 1 if rand > 0.5}}
+        @dish = Array.new(@x, 0) # 'spawn' an array with the right X dimension
+        (0...@x).each do |i| # fills the array with arrays to create a 2-d array
+            @dish[i] = Array.new(@y, 0)
+            (0...@y).each do |j|
+                @dish[i][j] = 1 if rand < @sparsity # randomness, but a defined fill percentage
+            end
+        end
+        printOut # print the newly made array
     end
-    
-    def printOut
-        0.upto(@x-1) {|i|
-            s = ''
-            0.upto(@y-1){ |j| s +=  ' '+ @dish[i][j].to_s}
-            puts s.gsub(/0/, " ").gsub(/1/, "0")
-
-        }
-                    @x.times {print"_"}
+    def printOut # this function prints the arrays contents into terminal in a pretty manner
+        (0...@y).each do |y|
+            row = ''
+            (0...@x).each do |x|
+                row += @dish[x][y].to_s
+            end
+            puts '|'+row.gsub(/0/, " ").gsub(/1/, "0")+'|' # makes cells into 0's and blanks into whitespace with borders
+        end
+        @x.times {print"_"} 
         puts ""
     end
-    
-    def generation
-        printOut
-        dishCopy = Marshal.load(Marshal.dump(@dish))
-        change = Marshal.load(Marshal.dump(@dish))
-        neighbors = [[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0]]
-        
-        0.upto(@x-1) {|i|
-            0.upto(@y-1){|j|
-                sum = 0
-                neighbors.each do |x|
-                         sum += dishCopy[i+x[0]][j+x[1]] unless dishCopy[i+x[0]] == nil or dishCopy[i+x[0]][j+x[1]] == nil
+    def generate(generations)# finds the next frame and prints current to terminal
+        (0..generations).each do |generation|
+            change = Marshal.load(Marshal.dump(@dish))# this takes the contents and completely extracts them, then loads them into a new object which we can edit worry free
+            neighbors = [[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0]] # the eight cells around a point to check for life
+            (0...@y).each do |y|
+                (0...@x).each do |x|# go to each cell
+                    sum = 0
+                    neighbors.each do |n|# check the neighbors
+                        sum += @dish[x+n[0]][y+n[1]] unless @dish[x+n[0]] == nil or @dish[x+n[0]][y+n[1]] == nil # careful around the edges, and sum the surroundings
+                    end
+                    change[x][y] = 0 if (sum >=4 or sum <= 2) # cells with 4 neighbors or more, or 2 or less are dead
+                    change[x][y] = @dish[x][y] if (sum == 2) #but if it is exactly 2, and it was already alive, it lives
+                    change[x][y] = 1 if (sum == 3) # 3 is always alive
                 end
-                change[i][j] = 0 if (sum >=4 or sum <= 2)
-                change[i][j] = dishCopy[i][j] if (sum == 2)
-                change[i][j] = 1 if (sum == 3)
-            }
-        }
-        
-        
-        @dish = Marshal.load(Marshal.dump(change))
+            end
+            @dish = Marshal.load(Marshal.dump(change)) #commit our changes
+            printOut# print new frame to terminal
+        end
     end
 end
 
-
-newGame = Game.new(100, 50)
-100.times {newGame.generation}
+newGame = Game.new
+newGame.generate(100)
 #fsdfs
